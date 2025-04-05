4f87ec1316cc4419a5df931f1a366db2df7de4f5
Megan van Bracht <megan.vanbracht@outlook.com>
2025-04-05 19:44:46 +1100

    project finished

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from stats import get_num_words
 from stats import character_count
 from stats import list_of_dictionaries
+import sys
 
-def get_book_text(path_to_file):
-	with open(path_to_file) as f:
+
+if len(sys.argv) != 2:
+	print("Usage: python3 main.py <path_to_book>")
+	sys.exit(1)
+else:
+	path_to_book = sys.argv[1]
+
+
+def get_book_text(path_to_book):
+	with open(path_to_book) as f:
 		file_contents = f.read()
 	return file_contents
 
 
 def main():
 	print("============ BOOKBOT ============")
-	print("Analyzing book found at books/frankenstein.txt...")
+	print(f"Analyzing book found at {path_to_book}...")
 	
 	print("----------- Word Count ----------")
-	text = get_book_text("./books/frankenstein.txt")
+	text = get_book_text(path_to_book)
 	word_count = get_num_words(text)
 	print(f"Found {word_count} total words")
 	
